b82073a2ef6c284678d5b078e16707b2d63a85e1
STAN-65031 <lox.ibany@gmail.com>
2022-08-28 21:58:12 +0300

    assignment with real numbers

---
+++ homeWork1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreatesArray(int str, int column)
+{
+    double[,] array = new double[str, column];
+    Random random = new Random();
+    for (int i = 0; i < str; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            double element = Convert.ToDouble(random.Next(-100, 100) / 10.0);
+            array[i, j] = element;
+        }
+
+
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] result = CreatesArray(m, n);
+PrintArray(result);
